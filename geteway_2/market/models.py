4dd66d1c43193ad40564a0173dddddb786968f58
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-08-15 05:56:24 +0330

    a simple cart market for test gateway

--- geteway_2/market/models.py
+++ geteway_2/market/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    product_name = models.CharField(max_length=250)
+    product_price = models.CharField(max_length=20)
+    quantity = models.IntegerField(default=1)
+
+    def total_price(self):
+        return self.product_price * self.quantity
