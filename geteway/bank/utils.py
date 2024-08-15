67838589cf595809fdf7bb7fa105d6c45fd1c23d
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-08-15 04:43:05 +0330

    create market app

--- geteway/bank/utils.py
+++ geteway/bank/utils.py
@@ -1,12 +1,11 @@
 import random
 import string
-# from .models import Account
+from .models import Account
 
 
 def create_account_number():
     prefix = '6037-9975'
     while True:
-        from .models import Account
         middle = ''.join(random.choices(string.digits, k=6))
         account_number = f'{prefix}-{middle[:4]}-{middle[4:]}'
         if not Account.objects.filter(account_number=account_number).exists():
